f62da9827a33316a6f76405af4b8b321ebfc3c15
Jason <jasonventresca@gmail.com>
2018-09-21 00:37:48 -0400

    refactoring

--- zen/scrape.py
+++ zen/scrape.py
@@ -4,15 +4,52 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def main():
-    resp = requests.get('https://www.dailyzen.com/quotes/archive/2015/01/P20')
+class Quote(object):
+    def __init__(self, text, author=None):
+        self.text = text
+        self.author = author
+
+    def __hash__(self):
+        return hash(self.text + self.author)
+
+    def __str__(self):
+        return '{}\n\n - {}'.format(
+            self.text,
+            self.author
+        )
+
+    def __repr__(self):
+        return '{}... - {}'.format(
+            self.text[:40],
+            self.author,
+        )
+
+
+def get_url(year, month, page):
+    month = str(month) if month > 9 else '0{}'.format(month) # TODO: is this necessary?
+    return 'https://www.dailyzen.com/quotes/archive/{year}/{month}/P{page}'.format(
+        year=year,
+        month=month,
+        page=page,
+    )
+
+
+def get_quotes(year, month, page):
+    url = get_url(year, month, page)
+    resp = requests.get(url)
     soup = BeautifulSoup(resp.text, 'html.parser')
-    quotes = soup.find_all('blockquote')
-    by = soup.find_all('cite')
+    quotes = [x.text for x in soup.find_all('blockquote')]
+    by = [x.text for x in soup.find_all('cite')]
     assert len(quotes) == len(by)
-    print('{}\n\n- by {}'.format(
-        quotes[0], by[0]
-    ))
+    for (text, author) in zip(quotes, by):
+        q = Quote(text, author)
+        yield q
+
+
+def main():
+    # 2015/01/P20
+    from pprint import pprint
+    pprint(tuple(get_quotes(year=2015, month=1, page=20)))
 
 
 if __name__ == '__main__':
