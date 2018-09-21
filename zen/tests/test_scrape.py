058bae61c2944f123c98b63c72f1fbae9a48b5ce
Jason <jasonventresca@gmail.com>
2018-09-21 00:58:08 -0400

    added todos

--- zen/tests/test_scrape.py
+++ zen/tests/test_scrape.py
@@ -2,12 +2,15 @@
 
 import sys
 from os.path import join, dirname
-sys.path.append(join(dirname(__file__), '..'))
 
+sys.path.append(join(dirname(__file__), '..'))
 from scrape import *
 
 
+# TODO: Do a similar test, where right before diff'ing observed vs. expected,
+#       we append a 'BROKEN' string onto observed, and assertRaises.
 def test_quote_extractions():
+    # TODO: Don't curl the website, instead pull from the checked in HTML file!
     for i, quote in enumerate(get_quotes(year=2015, month=1, page=20)):
         fname = join(
             dirname(__file__),
