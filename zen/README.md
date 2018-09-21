1c5971ae3dc55467622fb4a27e88809653596a6e
Jason <jasonventresca@gmail.com>
2018-09-20 23:44:28 -0400

    Putting together most of the scraper.

---
+++ zen/README.md
@@ -0,0 +1,3 @@
+To run the scraper:
+
+`pipenv run ./scrape.py`
--- zen/scrape.py
+++ zen/scrape.py
@@ -5,9 +5,14 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    html_doc = requests.get('https://www.dailyzen.com/quotes/archive/2015/01/P20')
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    print(soup)
+    resp = requests.get('https://www.dailyzen.com/quotes/archive/2015/01/P20')
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    quotes = soup.find_all('blockquote')
+    by = soup.find_all('cite')
+    assert len(quotes) == len(by)
+    print('{}\n\n- by {}'.format(
+        quotes[0], by[0]
+    ))
 
 
 if __name__ == '__main__':
