14649ee79028ab96c9b3b7a0913c71c7358145de
Jason <jasonventresca@gmail.com>
2018-10-26 05:50:11 +0000

    runner now logs to repo ;)

--- zen/send_all.sh
+++ zen/send_all.sh
@@ -4,7 +4,7 @@ set -eu
 # Can be installed via `crontab`, e.g.
 #       0 14 * * * /home/ubuntu/quotes/zen/send_all.sh
 
-(
-    cd $HOME/quotes/zen
-    pipenv run ./mailer.py
-)
+cd $HOME/quotes/zen
+pipenv run ./mailer.py \
+    1 >> ./send_all.out.log \
+    2 >> ./send_all.err.log
