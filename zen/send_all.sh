ea2135044beae0f5ca383a3b3aa4cbb8426da4d5
Jason <jasonventresca@gmail.com>
2018-11-03 03:59:44 +0000

    less magic in PATH and somehow i think this fixes the cron script

--- zen/send_all.sh
+++ zen/send_all.sh
@@ -5,12 +5,10 @@ set -eu
 #       0 14 * * * /home/ubuntu/quotes/zen/send_all.sh
 
 # Make sure we have everything necessary to run python 3 + pipenv. Cron does not have the same PATH by default as ubuntu's bash shell.
-export PATH=/home/ubuntu/dotfiles.jason_ventresca/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/Users/jasonventresca/Library/Python/3.7/bin:/home/ubuntu/.local/bin
+export PATH="$PATH":/home/ubuntu/.local/bin
 
 # Here's where our pipenv project lives
 cd $HOME/quotes/zen
 
 # Run it!
-pipenv run ./mailer.py \
-    1 >> ./send_all.out.log \
-    2 >> ./send_all.err.log
+pipenv run ./mailer.py >>./send_all.log 2>&1
